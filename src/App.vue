2612972da69e80def6a05acc6483974bfb2cdba2
saragenuardi <saretta.summer16@gmail.com>
2022-05-16 15:59:41 +0200

    aggiunto componente title + bonus hero

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <AppTitle />
+    <AppHero />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import AppTitle from "./components/AppTitle.vue";
+import AppHero from "./components/AppHero.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    AppTitle,
+    AppHero,
   },
 };
 </script>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <img src="../assets/img/banner.jpg" alt="banner" />
+    <h1>Questo è un banner</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppHero",
+};
+</script>
+
+<style scoped>
+img {
+  width: 100%;
+}
+</style>
